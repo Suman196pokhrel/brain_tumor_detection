7f85f89f245613f4e41370edeac5abc8ab1085cc
suman pokhrel <spokhrel196@gmail.com>
2021-10-09 13:02:06 +0530

    added data augmentation code

---
+++ mlModule/mlPrototype_01.py
@@ -0,0 +1,41 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+
+class DataGenerator:
+     def __init__(self,src_img,src_dir,batch_size=1,prefx_name="AugData",saved_img_format="jpg",no_of_images=50):
+
+          self.datagen = ImageDataGenerator(
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest')
+          self.src_image = src_img
+          self.src_dir = src_dir
+          self.batch_size = batch_size
+          self.perfix_name = prefx_name
+          self.saved_img_format = saved_img_format
+          self.no_of_img = no_of_images
+
+          self.img_path = self.src_image
+          self.img = load_img(self.img_path)
+
+          self.x = img_to_array(self.img)
+          self.x = self.x.reshape((1,) + self.x.shape)
+
+
+          i =0
+          for _ in self.datagen.flow(self.x, batch_size=1,save_to_dir=self.src_dir, save_prefix=self.perfix_name,save_format=self.saved_img_format):
+               i +=1
+               if i > self.no_of_img:
+                    break
+
+          
+
+
+if __name__ == "__main__":
+    DataGenerator(src_img=r"C:\Users\Suman pokhrel\OneDrive\Desktop\Final_Year_Project\btdCode\mlModule\testData\test1.jpeg",
+                         src_dir=r"C:\Users\Suman pokhrel\OneDrive\Desktop\Final_Year_Project\btdCode\mlModule\augumentedImages",
+                         no_of_images=10)
