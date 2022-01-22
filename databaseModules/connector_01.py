4b7a0da99e29bc68ec7535fd8bd4cf744cda28ab
Suman196pokhrel <spokhrel196@gmail.com>
2022-01-22 23:00:33 +0530

    connector_01 modified sucessfully for mariadb

--- databaseModules/connector_01.py
+++ databaseModules/connector_01.py
@@ -1,7 +1,11 @@
 # Imports
+from codecs import getencoder
 from dotenv import load_dotenv
 import os
-import mysql.connector
+import mariadb
+
+
+
 
 
 # Setting Up middlewares
@@ -10,9 +14,9 @@ load_dotenv()  # Loading the environment Variables
 
 class CustomConnector:
 
-	def __init__(self, host_name=os.getenv("HOST"), user_name=os.getenv("USER"), password=os.getenv("PASSWORD"), database_name="btd"):
+	def __init__(self, host_name=os.getenv("HOST"), user_name=os.getenv("USER"), password=os.getenv("PASSWORD"), database_name=os.getenv("DATABASE") ,port_num = int(os.getenv("PORT"))):
 
-		self.connection = mysql.connector.connect(host=host_name, user=user_name, passwd=password, database=database_name)
+		self.connection = mariadb.connect(host=host_name, user=user_name, password=password, database=database_name, port = port_num)
 		self.myCursor = self.connection.cursor(buffered=True)
 		print('Connection established')
 
